54369533fdf3cbaf06c666dd6016fedd3e5f5d0f
EvhenyiErokhin <erohin0611@gmail.com>
2022-09-15 06:13:04 +0300

    Verified, need refactoring

--- src/main/java/JdbcSqlConnector/Catalog.java
+++ src/main/java/JdbcSqlConnector/Catalog.java
@@ -2,11 +2,21 @@ package JdbcSqlConnector;
 
 import com.mysql.cj.jdbc.Driver;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Catalog {
+    private final static String QUERY_FIND_MY_BASE = "SELECT * FROM FirstDataBase.Veterinar";
+    private final static String QUERY_FIND_BY_BIKE = "SELECT * FROM FirstDataBase.Garage where Transport = 'Bike'";
+    private final static String QUERY_FIND_BY_PASSWORD = "SELECT * FROM FirstDataBase.UserTable WHERE Password LIKE '3%'";
+
     public static void main(String[] args) {
+
         String url = "jdbc:mysql://localhost:3306/mysql";
         String userName = "root";
         String password = "eev0507303539";
@@ -15,7 +25,7 @@ public class Catalog {
             DriverManager.registerDriver(new Driver());
             Connection connection = DriverManager.getConnection(url, userName, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM FirstDataBase.Veterinar");
+            ResultSet resultSet = statement.executeQuery(QUERY_FIND_MY_BASE);
 
             List<VetClinic> vetClinicList = new LinkedList<>();
             while (resultSet.next()) {
@@ -35,7 +45,7 @@ public class Catalog {
             DriverManager.registerDriver(new Driver());
             Connection connection = DriverManager.getConnection(url, userName, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM FirstDataBase.Garage where Transport = 'Bike'");
+            ResultSet resultSet = statement.executeQuery(QUERY_FIND_BY_BIKE);
 
             List<Garage> garageList = new LinkedList<>();
             while (resultSet.next()) {
@@ -54,7 +64,7 @@ public class Catalog {
             DriverManager.registerDriver(new Driver());
             Connection connection = DriverManager.getConnection(url, userName, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM FirstDataBase.UserTable WHERE Password LIKE '3%'");
+            ResultSet resultSet = statement.executeQuery(QUERY_FIND_BY_PASSWORD);
 
             List<UserTable> userTableList = new LinkedList<>();
             while (resultSet.next()) {
@@ -71,7 +81,7 @@ public class Catalog {
         }
     }
 
-    private static VetClinic buildVetClinic( ResultSet resultSet) throws SQLException {
+    private static VetClinic buildVetClinic(ResultSet resultSet) throws SQLException {
         VetClinic vetClinic = new VetClinic();
         vetClinic.setPet(resultSet.getString(1));
         vetClinic.setName(resultSet.getString(2));
@@ -80,7 +90,8 @@ public class Catalog {
         vetClinic.setCondition(resultSet.getString(5));
         return vetClinic;
     }
-    private static Garage buildGarage( ResultSet resultSet) throws SQLException {
+
+    private static Garage buildGarage(ResultSet resultSet) throws SQLException {
         Garage garage = new Garage();
         garage.setTransport(resultSet.getString(1));
         garage.setPassengers(resultSet.getInt(2));
@@ -88,7 +99,8 @@ public class Catalog {
         garage.setColor(resultSet.getString(4));
         return garage;
     }
-    private static UserTable buildUserTable( ResultSet resultSet) throws SQLException {
+
+    private static UserTable buildUserTable(ResultSet resultSet) throws SQLException {
         UserTable userTable = new UserTable();
         userTable.setUserLogin(resultSet.getString(1));
         userTable.setPassword(resultSet.getString(2));
